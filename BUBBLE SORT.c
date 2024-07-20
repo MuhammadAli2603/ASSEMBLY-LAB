2f1d61bea2c0b8dbccdc8c578e6b0312d9400433
MuhammadAli2603 <124609535+MuhammadAli2603@users.noreply.github.com>
2024-07-20 15:16:58 +0500

    Add files via upload

---
+++ BUBBLE SORT.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[4] = {10, 5, 9, 7};  // Define the array of size 4
+    int n = 4;  // Size of the array
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
